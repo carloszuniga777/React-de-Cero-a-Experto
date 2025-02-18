a8c69789c6a575fe8796eb2c29702f2f81bc7563
Carlos Zuniga <carloszuniga777@outlook.es>
2025-02-17 22:50:37 -0600

    modificaciones en variables de entorno

--- 06-my-journal-app/README.md
+++ 06-my-journal-app/README.md
@@ -25,3 +25,19 @@ VITE_FIREBASE_PROJECT_ID        = "projectId"
 VITE_FIREBASE_STORAGE_BUCKET    = "storageBucket"
 VITE_FIREBASE_MESSAGINGSENDERID = "messagingSenderId"
 VITE_FIREBASE_APP_ID            = "appId"
+```
+
+#### 1.3 Crear una cuenta en Cloudinary
+- Ingrese a [Cloudinary](https://cloudinary.com/) 
+
+##### 1.3.1 En el Archivo `.env`
+- Agregue las siguientes variables de entorno:
+
+```env
+ VITE_CLOUDINARY_CLOUD_NAME=your_cloud_name
+ VITE_CLOUDINARY_UPLOAD_PRESET=your_upload_preset
+```
+
+- **your_cloud_name:** Es el Cloud Name obtenido en la seccion del Dashboard de Cloudinary
+- **your_upload_preset:** Es el Preset obtenido en la seccion configuraciones --> subir, es necesario crear un nuevo preset y configurarlo en modo: Unsigned y ponerle un nombre tanto para el preset como al Asset Folder, el nombre que se le de al preset es el que va a utilizar como variable de entorno VITE_CLOUDINARY_UPLOAD_PRESET  
+
--- 06-my-journal-app/src/helpers/fileUpload.js
+++ 06-my-journal-app/src/helpers/fileUpload.js
@@ -1,3 +1,7 @@
+// Obtener variables de entorno
+const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+
 
 /*
   Nomeclatura: [POST] https://api.cloudinary.com/v1_1/:cloud_name/:action
@@ -11,6 +15,7 @@
   Uploading files with react.js:  https://www.pluralsight.com/resources/blog/guides/uploading-files-with-reactjs
 */
 
+
 //Funcion para subir las imagenes al CLoudinary
 export const fileUpload = async(file) =>{
 
@@ -18,11 +23,11 @@ export const fileUpload = async(file) =>{
 
     // URL base de la API de Cloudinary para subir archivos. 
     // cloud_name: dhfivi1ye (nombre de tu cuenta en Cloudinary)
-    const cloudURL = 'https://api.cloudinary.com/v1_1/dhfivi1ye/upload'
+    const cloudURL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/upload`
 
     // Creamos un objeto FormData para enviar los parámetros requeridos por Cloudinary
     const formData = new FormData()
-    formData.append('upload_preset', 'JournalApp-Curso-React')      //upload_preset: nombre del 'upload preset' de cloudinay | 'JournalApp-Curso-React' debe coincidir exactamente con el nombre de tu preset
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)      //upload_preset: nombre del 'upload preset' de cloudinay | coincidir exactamente con el nombre de tu preset
     formData.append('file', file)        
 
     try {
@@ -43,3 +48,21 @@ export const fileUpload = async(file) =>{
         throw new Error('Ocurrio un erro al momento de subir el archivo a cloudynary', error.message)
     }                           
 }
+
+
+
+//Crear un archivo .env y buscar las configuraciones de cloudinary (https://console.cloudinary.com/): 
+/**
+ * Con las variables de entorno:
+ * 
+    VITE_CLOUDINARY_CLOUD_NAME=your_cloud_name
+    VITE_CLOUDINARY_UPLOAD_PRESET=your_upload_preset
+
+    -your_cloud_name: Es el Cloud Name obtenido en la seccion del Dashboard de Cloudinary
+    -your_upload_preset: Es el Preset obtenido en la seccion configuraciones --> subir, 
+                        es necesario crear un nuevo preset y configurarlo en modo: Unsigned 
+                        y ponerle un nombre tanto para el preset como al Asset Folder, 
+                        el nombre que se le de al preset es el que va a utilizar como variable de entorno
+                        VITE_CLOUDINARY_UPLOAD_PRES
+ * 
+ */
